9bb0840d42bb2c50a3dc6f8677b87d422522a8e4
mcallaghan <max.w.callaghan@gmail.com>
2016-03-06 14:17:29 +0100

    first commit

---
+++ get_followers.R
@@ -0,0 +1,27 @@
+library("twitteR")
+library("igraph")
+source("define_keys.R")
+
+
+setup_twitter_oauth(
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET
+)
+
+accounts <- c("realDonaldTrump","HillaryClinton")
+
+retry <- 1000000
+
+s <- Sys.time()
+for (a in accounts) {
+  followerIDs <- getUser(a)$getFollowerIDs(retryOnRateLimit=10)
+  followers <- data.frame(followerIDs=followerIDs)
+  saveRDS(followers, file=paste0(a,".Rda"))
+}
+e <- Sys.time()
+
+print(e-s)
+
+
