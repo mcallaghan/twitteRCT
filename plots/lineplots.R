1b56b6156c897e3c1b60db4b328ec1e7bc0d3492
mcallaghan <max.w.callaghan@gmail.com>
2016-05-11 14:24:28 +0200

    graphs

--- plots/lineplots.R
+++ plots/lineplots.R
@@ -21,21 +21,10 @@ for (d in unique(user_days$date)) {
   print(d)
 }
 
-# tweet_examples$daye <- lead(tweet_examples$day)
-# 
-# tweet_days <- tweet_examples %>%
-#   select(tweet_no,truth,day,daye) %>%
-#   mutate(
-#     day = day+0.1,
-#     daye = daye-0.1
-#   ) %>%
-#   gather(se,day,-tweet_no,-truth) %>%
-#   mutate(
-#     ys = 0.2
-#   )
-# 
-# 
-# tweet_days <- rbind(tweet_days,mutate(tweet_days,ys=0.4,day=rev(day)))
+tweet_examples$daye <- lead(tweet_examples$day)
+
+tweet_examples[is.na(tweet_examples$daye),"daye"] <- tweet_examples[is.na(tweet_examples$daye),"day"] + 2
+
 
 depvars <- c("like_n","tweet_n","trump_rt_n",
                         "trump_mention_n","MAGA_n",
@@ -44,26 +33,65 @@ depvars <- c("like_n","tweet_n","trump_rt_n",
 likes <- user_days %>% 
   group_by(t_group,day) %>%
   summarise(
-    average_likes = mean(like_n),
-    sem = sd(like_n)/sqrt(length(like_n))
+    m = mean(like_n),
+    sem = sd(like_n)/sqrt(length(like_n)),
+    uci = m + sem,
+    lci = m - sem
   ) 
 
-yvar = 'average_likes'
+maga <- user_days %>% 
+  group_by(t_group,day) %>%
+  summarise(
+    m = mean(MAGA_n),
+    sem = sd(MAGA_n)/sqrt(length(MAGA_n)),
+    uci = m + sem,
+    lci = m - sem
+  ) 
 
 pd <- position_dodge(0.5)
 
+w <- 8
+h <- 5
+
 ggplot(data=likes) +
   geom_line(
-    aes_string('day',yvar,colour='t_group',group='t_group')
+    aes(day,m,colour=t_group,group=t_group)
   ) +
-  geom_errorbar(
-    aes(x=day,ymin=average_likes-sem, ymax=average_likes+sem,group=t_group,colour=t_group), 
-    width=.1,
-    position=pd
-    ) +
+  geom_line(
+    aes(day,uci,colour=t_group,group=t_group),
+    alpha=0.5,
+    linetype=2
+  ) + 
+  geom_line(
+    aes(day,lci,colour=t_group,group=t_group),
+    alpha=0.5,
+    linetype=2
+  ) +
   scale_x_continuous(breaks=seq(1,i,by=1)) +
+  scale_colour_discrete(guide = guide_legend(title = "Treatment\nGroup")) +
   labs(x="Day", y="Avg. Likes") +
-  theme_bw() 
+  theme_bw() +
+ggsave("plots/likes.png",width=w,height=h,dpi=500)
 
+
+ggplot(data=maga) +
+  geom_line(
+    aes(day,m,colour=t_group,group=t_group)
+  ) +
+  geom_line(
+    aes(day,uci,colour=t_group,group=t_group),
+    alpha=0.5,
+    linetype=2
+  ) + 
+  geom_line(
+    aes(day,lci,colour=t_group,group=t_group),
+    alpha=0.5,
+    linetype=2
+  ) +
+  scale_x_continuous(breaks=seq(1,i,by=1)) +
+  scale_colour_discrete(guide = guide_legend(title = "Treatment\nGroup")) +
+  labs(x="Day", y="Avg. #MakeAmericaGreatAgain") +
+  theme_bw() 
+ggsave("plots/maga.png",width=w,height=h)
 
 
