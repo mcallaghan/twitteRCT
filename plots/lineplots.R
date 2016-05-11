28fe78703467b6f167c610a164dcde0ae1951b67
mcallaghan <max.w.callaghan@gmail.com>
2016-05-11 13:27:24 +0200

    add tables

--- plots/lineplots.R
+++ plots/lineplots.R
@@ -10,3 +10,51 @@ sourceDirectory("functions/",modifiedOnly = FALSE)
 user_days <- read.csv("data/user_days.csv")
 
 load("data/tweet_examples.Rdata")
+load("data/group_avs.Rdata")
+
+i <- 0
+for (d in unique(user_days$date)) {
+  i <- i + 1
+  user_days[user_days$date==d,"day"] <- i
+  group_avs[group_avs$date==d,"day"] <- i
+  tweet_examples[tweet_examples$`start date`==d,"day"] <- i
+  print(d)
+}
+
+yvar = 'average_likes'
+
+tweet_examples$daye <- lead(tweet_examples$day)
+
+tweet_days <- tweet_examples %>%
+  select(tweet_no,truth,day,daye) %>%
+  mutate(
+    day = day+0.1,
+    daye = daye-0.1
+  ) %>%
+  gather(se,day,-tweet_no,-truth) %>%
+  mutate(
+    ys = 0.2
+  )
+
+
+tweet_days <- rbind(tweet_days,mutate(tweet_days,ys=0.4,day=rev(day)))
+
+
+p <- ggplot() +
+  geom_line(
+    data=group_avs,
+    aes_string('day',yvar,colour='t_group',group='t_group')
+  ) +
+  geom_polygon(
+    data=tweet_days,
+    aes(x=day,y=ys,group=tweet_no,width=15),
+    #size=1,
+    alpha=0.8
+  ) + 
+  scale_x_continuous(breaks=seq(1,i,by=1)) +
+  theme_bw() 
+
+p
+
+
+
---
+++ tables/tweets.R
@@ -0,0 +1,58 @@
+library(RMySQL)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(plm)
+library(R.utils)
+library(xtable)
+
+sourceDirectory("functions/",modifiedOnly = FALSE)
+
+load("data/sent_tweets.Rdata")
+
+tweet_examples <- sent_tweets %>%
+  group_by(tweet_no) %>%
+  arrange(PID) %>%
+  filter(row_number()==1) %>%
+  select(text,truth,day) %>%
+  rename(
+    `start date` = day
+  ) %>%
+  mutate(
+    tlength = nchar(text),
+    nspaces = length(strsplit(text," ")[[1]]),
+    nmid = nspaces/2,
+    nmid1 = nmid + 1,
+    w1 = paste(strsplit(text," ")[[1]][1:nmid],collapse=" "),
+    w2 = paste(strsplit(text," ")[[1]][nmid1:nspaces],collapse=" "),
+    broken_text = paste(w1,w2,sep="\n"),
+    start_date_adj = ifelse(
+      `start date` == "2016-04-30",
+      "2016-05-01",
+      `start date`
+    )
+  ) %>%
+  ungroup() %>%
+  rename(
+    `Tweet number` = tweet_no,
+    Text = text,
+    Truth = truth,
+    `Start date` = `start date`
+  )
+
+print(xtable(
+  select(tweet_examples,`Tweet number`,Text,Truth,`Start date`),
+  align=c(
+    "c",
+    "c",
+    "p{8cm}",
+    "c",
+    "c"),
+  caption="Example tweets",
+  label="table:example_tweets",
+  table.placement="!h"
+),
+include.rownames=FALSE,
+comment=FALSE,
+file="tables/sent_tweets.tex"
+)
